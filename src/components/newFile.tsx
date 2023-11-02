c3da2496546f84fab6790241f106b2dfbeebd316
= <abhisingh30800@gmail.com>
2023-11-02 18:15:01 +0530

    added about section

---
+++ src/components/newFile.tsx
@@ -0,0 +1,18 @@
+<Fragment>
+<section class="section about-section">
+<h2>About the Platform</h2>
+<div class="feature-wrapper">
+<div class="feature">
+<img src="./nft/features-1.png" alt="" />
+<div class="feature-info">
+<div class="feature-text">
+
+<h3>Create your own NFT</h3>
+<p>Multiple Chains, one Home. Stack up all your NFTs from across blockchains</p>
+</div>
+</div>
+.feature-states
+</div>
+</div>
+</section>
+</Fragment>;
